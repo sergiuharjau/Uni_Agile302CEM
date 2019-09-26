01fc578a7aa61edab6242a801669ab68c6624ad9
Peter Robertson <rober380@uni.coventry.ac.uk>
2019-09-26 12:45:46 +0100

    fixed indentation

--- database-demo/index.js
+++ database-demo/index.js
@@ -1,28 +1,29 @@
 const Database = require("./modules/Database")
 
 async function getAllData () {
-  var database = await new Database();
-  await database.getAllSensorData().then( data => {
-    data.forEach(element => {
-      console.log(element._id.getTimestamp())
-      console.log(element.sensorName)
-      console.log(element.location)
-      console.log(element.value.toString())
-      console.log(element.dateRecorded)
-      console.log("*******************************")
-    });
-  })
+	var database = await new Database();
+	await database.getAllSensorData()
+	.then( data => {
+		data.forEach(element => {
+		console.log(element._id.getTimestamp())
+		console.log(element.sensorName)
+		console.log(element.location)
+		console.log(element.value.toString())
+		console.log(element.dateRecorded)
+		console.log("*******************************")
+		});
+	})
 }
 
 async function insertSensorData () {
-  var database = await new Database();
-  var exampleJSON = {
-    sensorName: "temp-2",
-    location: "Lounge",
-    value: 16,
-    dateRecorded: new Date()
-  }
-  await database.insertSensorData(exampleJSON)
+	var database = await new Database();
+	var exampleJSON = {
+		sensorName: "temp-2",
+		location: "Lounge",
+		value: 16,
+		dateRecorded: new Date()
+	}
+	await database.insertSensorData(exampleJSON)
 }
 
 getAllData()
