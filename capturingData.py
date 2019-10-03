ad19136c962ad2fe541a1fc32f6487c0d2f0e07f
harjaus <harjaus@coventry.ac.uk>
2019-10-03 14:52:20 +0100

    Quick script to capture all mqtt data onto sqlite3

---
+++ capturingData.py
@@ -0,0 +1,36 @@
+import paho.mqtt.client as mqtt #import the client1
+global db 
+
+def on_message(client, userdata, msg):
+    print(msg.topic+":\n"+str(msg.payload.decode()))
+    #insertIntoSQL(msg.payload)
+
+def insertIntoSQL(data):
+    sql = ''' INSERT INTO data(sensorname, location, value, dateRecorded)
+              VALUES(?,?,?,?) '''
+    cur = db.cursor()
+    cur.execute(sql, data["label"], data["location"], data["value"], data["time"])
+
+def runMQTT():
+    broker_address = "mqtt.coventry.ac.uk" 
+    broker_port = 8883
+
+    user = "302CEM"
+    password = "n3fXXFZrjw"
+
+    client = mqtt.Client() #create new instance
+
+    client.username_pw_set(user, password)
+
+    client.on_message = on_message
+    client.tls_set("/home/serge/mqtt.crt")
+    client.connect(broker_address, broker_port) #connect to broker
+
+    client.subscribe("302CEM/placeholder/#")
+
+    client.loop_forever()
+
+if __name__ == "__main__":
+    global db 
+    #db = sqlite3.connect("pathToFile")
+    runMQTT() # this runs forever
