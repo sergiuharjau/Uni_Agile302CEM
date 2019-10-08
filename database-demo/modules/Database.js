fb57459a007328d27c43a7b8444b95e995183f7f
Peter Robertson <rober380@uni.coventry.ac.uk>
2019-10-08 17:29:48 +0100

    database can be recreated from scratch for testing purposes

--- database-demo/modules/Database.js
+++ database-demo/modules/Database.js
@@ -4,20 +4,27 @@ const sqlite = require('sqlite-async')
 module.exports = class database {
 	constructor(dbName=':memory:') {
 		return (async() => {
-			this.db = await sqlite.open(dbName);
-			if (dbName === ':memory:') {
-				console.log("data in memory")
-				//TODO: Insert script to create database from scratch
-				const sql = 'PRAGMA foreign_keys = off; BEGIN TRANSACTION;  DROP TABLE IF EXISTS data;  CREATE TABLE data (     sensorName   VARCHAR (32) NOT NULL,     location     VARCHAR (32) NOT NULL ON CONFLICT ROLLBACK,     value        VARCHAR (8)  NOT NULL ON CONFLICT ROLLBACK,     dateRecorded DATETIME     NOT NULL ON CONFLICT ROLLBACK,     dateCreated  DATETIME     DEFAULT (CURRENT_TIMESTAMP),     PRIMARY KEY (         sensorName,         dateRecorded     ) );   COMMIT TRANSACTION; PRAGMA foreign_keys = on; '
-				await this.db.run(sql)
+			try {
+				this.db = await sqlite.open(dbName);
+				if (dbName === ':memory:') {
+					console.log("data in memory")
+					//TODO: Insert script to create database from scratch
+					var sql = 'CREATE TABLE sensors (sensorName VARCHAR (32) UNIQUE CONSTRAINT PK_sensors PRIMARY KEY NOT NULL, type VARCHAR (16) NOT NULL, location VARCHAR (16) NOT NULL, dateAdded DATETIME DEFAULT (CURRENT_TIMESTAMP));'
+					await this.db.run(sql)
+					var sql = 'CREATE TABLE data (sensorName   VARCHAR (32) NOT NULL CONSTRAINT FK_Sensors_data REFERENCES sensors (sensorName), value VARCHAR (8)  NOT NULL ON CONFLICT ROLLBACK, dateRecorded VARCHAR (32) NOT NULL, dateCreated DATETIME DEFAULT (CURRENT_TIMESTAMP), PRIMARY KEY(sensorName, dateRecorded));'
+					await this.db.run(sql)
+				}
+				return this
+			} catch (error) {
+				console.log(error)
 			}
-			return this
+			
 		})()
 	}
 
 	async getAllSensorData() {
 		try {
-			var sql = `SELECT s.sensorName, s.location, d.value, d.dateRecorded FROM sensors s, data d WHERE d.sensorName = s.sensorName;`
+			var sql = `SELECT s.sensorName, s.location, d.value, d.dateRecorded FROM sensors s, data d WHERE d.sensorName = s.sensorName`
 			return await this.db.all(sql)
 		} catch (err){
 			return err
