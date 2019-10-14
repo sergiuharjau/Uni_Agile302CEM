03be774005f8592dd167d4661d14f06becabe95a
Peter Robertson <rober380@coventry.ac.uk>
2019-10-14 13:40:51 +0100

    ability to get min, average, max for sensors over a period of time

--- database-demo/index.js
+++ database-demo/index.js
@@ -19,6 +19,20 @@ async function getDataRange (startDate,endDate) {
 	await PrintData(data)
 }
 
+async function latestReading () {
+	var database = await new Database(databasePath)
+	var data = await database.latestReading()
+	await PrintData(data)
+}
+
+async function getStatistics () {
+	var database = await new Database(databasePath)
+	// var listOfSensors = "'temp2'"
+	var listOfSensors = 'NULL'
+	var data = await database.getStatistics(listOfSensors, 'NULL', 'NULL')
+	console.log(data)
+}
+
 async function PrintData (JSONData) {
 	JSONData.forEach(element => {
 		console.log("Sensor Name is " + element.sensorName)
@@ -29,8 +43,14 @@ async function PrintData (JSONData) {
 	});
 }
 
-// getAllData();
-// getTodaysData();
-const date = new Date(2019,09,01,00,00,00,00);
-const todaysDate = new Date();
-getDataRange(date, todaysDate)
+// getAllData()
+
+// getTodaysData()
+
+// const date = new Date(2019,09,01,00,00,00,00)
+// const todaysDate = new Date()
+// getDataRange(date, todaysDate)
+
+// latestReading();
+
+getStatistics();
