94e6cafb74044228e51bb626fedd4d6f323a8bee
harjaus <harjaus@coventry.ac.uk>
2019-10-22 17:07:53 +0100

    Added new script to call shell commands

--- database-demo/modules/execute.js
+++ database-demo/modules/execute.js
@@ -1,14 +1,33 @@
 #!/usr/bin/env node
 
 function execute(command) {
-    const exec = require('child_process').exec
+  const exec = require('child_process').exec
   
-    exec(command, (err, stdout, stderr) => {
+    await exec(command, (err, stdout, stderr) => {
       console.log(err, stdout, stderr)
       return(stdout)
     })
   }
   
+async function sh(cmd) {
+  const exec = require('child_process').exec
+  
+    return new Promise(function (resolve, reject) {
+      exec(cmd, (err, stdout, stderr) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(stdout);
+        }
+      });
+    });
+  }
+
+let output = sh("python3 /home/pi/Documents/AgilePlaceholder/capturing_test.py")
+
+console.log(output)
+
 module.exports = {
-    execute
+    execute,
+    sh
 }
